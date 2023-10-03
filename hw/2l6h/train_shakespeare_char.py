8ed343c82761facf448fb81de18df458b5ce4aeb
Hafeez Ali <hafeez.ali5@gmail.com>
2023-10-03 17:09:56 +0000

    Modified params for 2l models

--- hw/2l6h/train_shakespeare_char.py
+++ hw/2l6h/train_shakespeare_char.py
@@ -1,7 +1,7 @@
 # train a miniature character-level shakespeare model
 # good for debugging and playing on macbooks and such
 
-out_dir = 'out-shakespeare-char'
+out_dir = 'out-shakespeare-char-2l6h'
 eval_interval = 250 # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 10 # don't print too too often
@@ -19,14 +19,14 @@ batch_size = 64
 block_size = 256 # context of up to 256 previous characters
 
 # baby GPT model :)
-n_layer = 6
+n_layer = 2
 n_head = 6
 n_embd = 384
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+max_iters = 3000
+lr_decay_iters = 3000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
